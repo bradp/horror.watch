30ee4dba0e018fec8fdb45e9cada468ebd6690a3
Brad Parbs <brad@bradparbs.com>
2021-01-04 09:15:07 -0600

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,19 +2,19 @@ document.addEventListener( 'DOMContentLoaded', function() {
 	let modalTrigger = document.getElementById( 'watch-trigger' );
 
 	modalTrigger.addEventListener( 'click', (event) => {
-			event.preventDefault();
-			document.getElementById( 'watch' ).classList.remove( 'hidden' );
-			videojs( 'video-el' ).play();
-
-		} );
+		event.preventDefault();
+		document.getElementById( 'watch' ).classList.remove( 'hidden' );
+		videojs( 'video-el' ).play();
+	} );
 } );
 
 document.addEventListener( 'DOMContentLoaded', function() {
 	let modal = document.getElementById( 'watch' );
 	let overlay = document.getElementById( 'watch-overlay' );
 
-	overlay.addEventListener( 'click', function() {
-		modal.classList.add( 'hidden' );
-		videojs( 'video-el' ).pause();
+	overlay.addEventListener( 'click', (event) => {
+		event.preventDefault();
+		modal.classList.add('hidden');
+		videojs('video-el').pause();
 	} );
 } );
