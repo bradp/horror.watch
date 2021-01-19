bc871fe5242ef8469cb021216f5ba61dce809a8a
Brad Parbs <brad@bradparbs.com>
2021-01-19 12:00:32 -0600

    Fix JS error

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,24 @@
 document.addEventListener( 'DOMContentLoaded', function() {
 	let modalTrigger = document.getElementById( 'watch-trigger' );
 
-	modalTrigger.addEventListener( 'click', (event) => {
-		event.preventDefault();
-		document.getElementById( 'watch' ).classList.remove( 'hidden' );
-		videojs( 'video-el' ).play();
-	} );
+	if ( modalTrigger ) {
+		modalTrigger.addEventListener( 'click', (event) => {
+			event.preventDefault();
+			document.getElementById( 'watch' ).classList.remove( 'hidden' );
+			videojs( 'video-el' ).play();
+		} );
+	}
 } );
 
 document.addEventListener( 'DOMContentLoaded', function() {
 	let modal = document.getElementById( 'watch' );
 	let overlay = document.getElementById( 'watch-overlay' );
 
-	overlay.addEventListener( 'click', (event) => {
-		event.preventDefault();
-		modal.classList.add('hidden');
-		videojs('video-el').pause();
-	} );
+	if ( overlay ) {
+		overlay.addEventListener( 'click', (event) => {
+			event.preventDefault();
+			modal.classList.add('hidden');
+			videojs('video-el').pause();
+		} );
+	}
 } );
